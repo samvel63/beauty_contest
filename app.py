da8127ff679f76cf0699987b3eccbe858ca26ccc
samkhit1 <samkhit1@mts.ru>
2020-03-12 23:19:25 +0300

    denis is sucking a penis

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ def save_faces(image_path):
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+    height, width, channels = image.shape
+
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
     faces = face_cascade.detectMultiScale(
         gray,
@@ -34,18 +36,30 @@ def save_faces(image_path):
 
     for ind, face in enumerate(faces):
         x, y, w, h = [v for v in face]
-        face_image = image[y:(y+h+50), x:(x+w+50)]
+        x_inc = int(w*0.35)
+        y_inc = int(h*0.35)
 
+        x0, x1, y0, y1 = x, x+w, y, y+h
+
+        if x-x_inc > 0:
+            x0 -= x_inc
+        if x1+x_inc < width:
+            x1 += x_inc
+        if y-y_inc > 0:
+            y0 -= y_inc
+        if y1+y_inc < height:
+            y1 += y_inc
+
+        face_image = image[y0:(y1+y_inc), x0:(x1+x_inc)]
         face_image_name = f'face_{sum_images}_{ind}.1.jpg'
 
-        resized = cv2.resize(face_image, (100, 100), interpolation=cv2.INTER_AREA)
+        resized = cv2.resize(face_image, (75, 75), interpolation=cv2.INTER_AREA)
         status = cv2.imwrite(f'faces/{face_image_name}', resized)
 
         print(f'[INFO] Image {face_image_name} written to filesystem: ', status)
 
     if len(faces) > 0:
         sum_images += 1
-
 
 
 if __name__ == '__main__':

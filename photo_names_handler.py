0b07bbbcd099366612f7611cfce8dd91c69c8156
samkhit1 <samkhit1@mts.ru>
2020-03-22 23:45:07 +0300

    Fix photo_names_handler.py and add more mualattos

--- photo_names_handler.py
+++ photo_names_handler.py
@@ -1,4 +1,6 @@
 import argparse
+import random
+import string
 import os
 
 
@@ -11,8 +13,22 @@ def parse_args():
     return parser.parse_args()
 
 
+def random_string(string_length=10):
+    return ''.join(random.choice(string.ascii_lowercase) for i in range(string_length))
+
+
 def main():
     args = parse_args()
+
+    photos_list = os.listdir(args.path)
+    random_name = random_string()
+    for ind, photo_name in enumerate(photos_list):
+        new_name = f'{random_name}.{ind+1}.jpg'
+
+        before = os.path.join(args.path, photo_name)
+        after = os.path.join(args.path, new_name)
+
+        os.rename(before, after)
 
     photos_list = os.listdir(args.path)
     for ind, photo_name in enumerate(photos_list):

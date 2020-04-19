ab06acbbe6d4d19c7716b2a3bd400da0402f67c2
dmbobyl1 <dmbobyl1@mts.ru>
2020-04-19 15:25:19 +0300

    prefinal

--- predictor.py
+++ predictor.py
@@ -3,6 +3,8 @@ import sys
 import logging
 
 import cv2
+import numpy as np
+import tensorflow as tf
 
 
 root_path = os.path.dirname(os.path.abspath(__file__))
@@ -10,8 +12,30 @@ accounts_path = os.path.join(root_path, 'ACCOUNTS')
 success_path = os.path.join(root_path, 'SUCCESS')
 failed_path = os.path.join(root_path, 'FAILED')
 
+binary_model_path = os.path.join(root_path, 'binary_filter/models/binary_filter_model_1587241639.h5')
+categorizer_model_path = os.path.join(root_path, 'categorizer/models/my_model.h5')
+
 image_size_width = 150
 image_size_height = 150
+
+IMG_HEIGHT = 150
+IMG_WIDTH = 150
+
+
+binary_filter_classes = ['GIRL', 'LIT']
+categorizer_classes = ['ASIAN', 'BRUNETTE', 'DREADLOCK', 'GINGER', 'MULATTO']
+
+
+
+
+app_name = os.path.splitext(os.path.basename(__file__))[0]
+
+logger = logging.getLogger(app_name)
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s %(name)s: %(message)s', datefmt='%y/%m/%d %H:%M:%S')
+streamHandler = logging.StreamHandler()
+streamHandler.setFormatter(formatter)
+logger.addHandler(streamHandler)
+logger.setLevel(logging.INFO)
 
 
 
@@ -19,13 +43,9 @@ def get_paths(path):
     return sorted(os.path.join(path, file) for file in os.listdir(path))
 
 
-
-
-
-
 def find_faces(account_path):
     
-    def get_resized_face(face):
+    def get_resized_face(face, image, height, width):
         x, y, w, h = (v for v in face)
         x_inc = int(w*0.35)
         y_inc = int(h*0.35)
@@ -43,15 +63,18 @@ def find_faces(account_path):
 
         face_image = image[y0:(y1+y_inc), x0:(x1+x_inc)]
         face_image_resized = cv2.resize(face_image, (image_size_width, image_size_height), interpolation=cv2.INTER_AREA)    
-
         return face_image_resized
 
+    logger.info(f'Handling account \'{os.path.basename(account_path)}\'')
+    
     found_faces = []
-    for file_path in get_paths(account_path):
-        image_name = os.path.basename(image_path)
-        logger.info(f'Read {image_name}')
 
+    file_paths = get_paths(account_path)
+    logger.info(f'Found {len(file_paths)} photos in account \'{os.path.basename(account_path)}\'')
+
+    for image_path in get_paths(account_path):
         image = cv2.imread(image_path)
+        height, width, channels = image.shape
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
         face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
@@ -61,115 +84,58 @@ def find_faces(account_path):
             minNeighbors=3,
             minSize=(80, 80)
         )
-        logger.info(f'Found {len(faces)} faces for {image_name}!')
-
         for face in faces:
-            found_faces.append(get_resized_face(face))
-        
-    return found_faces
+            found_faces.append(get_resized_face(face, image, height, width))
     
+    logger.info(f'Found {len(found_faces)} faces in account \'{os.path.basename(account_path)}\'')
+    return found_faces
+
+
+
+def get_prediction(model, image):
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image_tensor = tf.convert_to_tensor(image_rgb, dtype=tf.float32)
+    image_tensor = tf.image.resize(image_tensor, [IMG_HEIGHT, IMG_WIDTH])
+    image_tensor /= 255.0
+    image_tensor = np.reshape(image_tensor, [1, IMG_HEIGHT, IMG_WIDTH, 3])
+    return model.predict_classes(image_tensor)[0]
+
 
 def main():
-    for account_path in account_paths:
+
+    binary_filter = tf.keras.models.load_model(binary_model_path)
+    binary_filter.summary()
+
+    categorizer = tf.keras.models.load_model(categorizer_model_path)
+    categorizer.summary()
+
+    accounts_paths = get_paths(accounts_path)
+    logger.info(f'Found {len(accounts_paths)} girls')
+
+    for account_path in accounts_paths:
         faces = find_faces(account_path)
-        faces_size = len(faces)
-        
+
         lit_counter = 0
-        predictions_statistics = [0] * len(classes)
+        predictions_statistics = [0] * len(categorizer_classes)
         
         for face in faces:
-            if binary_filter_get_prediction(face) == CLASS_LIT:
+            if binary_filter_classes[get_prediction(binary_filter, face)[0]] == 'LIT':
                 lit_counter += 1
                 continue
             
-            predictions_statistics[categorizer_get_prediction(face)] += 1
+            predictions_statistics[get_prediction(categorizer, face)] += 1
 
-        if lit_counter == faces_size:
-            move_to_failed(account_path)
+        if lit_counter == len(faces):
+            logger.info(f'Move account \'{os.path.basename(account_path)}\' to FAILED')
+            # move_account(account_path, 'FAILED')
         else:
             intensified_prediction = predictions_statistics.index(max(predictions_statistics))
-            move_to_success(account_path, intensified_prediction)
+            logger.info(f'Account \'{os.path.basename(account_path)}\' belongs to \'{categorizer_classes[intensified_prediction]}\' class')
+
+            #move_account(account_path, 'SUCCESS', intensified_prediction)
 
 
-        
-
-            
-
-                
-
-
-
-
-
-
-
-def save_faces(image_path, output_path):
-    global sum_images
-
-    image_name = os.path.basename(image_path)
-    logger.warning(f'Read {image_name}')
-
-    image = cv2.imread(image_path)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    height, width, channels = image.shape
-
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    faces = face_cascade.detectMultiScale(
-        gray,
-        scaleFactor=1.3,
-        minNeighbors=3,
-        minSize=(80, 80)
-    )
-    logger.warning(f'Found {len(faces)} faces for {image_name}!')
-
-    for ind, face in enumerate(faces):
-        x, y, w, h = (v for v in face)
-        x_inc = int(w*0.35)
-        y_inc = int(h*0.35)
-
-        x0, x1, y0, y1 = x, x+w, y, y+h
-
-        if x-x_inc > 0:
-            x0 -= x_inc
-        if x1+x_inc < width:
-            x1 += x_inc
-        if y-y_inc > 0:
-            y0 -= y_inc
-        if y1+y_inc < height:
-            y1 += y_inc
-
-        face_image = image[y0:(y1+y_inc), x0:(x1+x_inc)]
-        face_image_resized = cv2.resize(face_image, (image_size_width, image_size_height), interpolation=cv2.INTER_AREA)
-
-        
-
-
-def main():
-    input_path = sys.argv[1]
-    output_path = sys.argv[2]
-    logger.warning(f'Searching for faces in {input_path} and saving to {output_path}')
-
-    for file_path in get_paths(input_path):
-        try:
-            save_faces(file_path, output_path)
-        except Exception as e:
-            logger.error(e)
 
 
 if __name__ == '__main__':
-    sum_images = 1
-    app_name = os.path.splitext(os.path.basename(__file__))[0]
-
-    logger = logging.getLogger(app_name)
-    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s %(name)s: %(message)s', datefmt='%y/%m/%d %H:%M:%S')
-    streamHandler = logging.StreamHandler()
-    streamHandler.setFormatter(formatter)
-    logger.addHandler(streamHandler)
-    logger.setLevel(logging.INFO)
-
-    if len(sys.argv) != 3:
-        logger.error(f'Bad input parameters: `{app_name}.py input_path output_path`')
-        exit(0)
-
     main()

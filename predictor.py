a17bc4250153a337c071bf15b46da909fc64f9e1
dmbobyl1 <dmbobyl1@mts.ru>
2020-04-19 17:35:55 +0300

    upd

--- predictor.py
+++ predictor.py
@@ -1,46 +1,19 @@
 import os
 import sys
 import logging
+import shutil
+import argparse
 
 import cv2
 import numpy as np
 import tensorflow as tf
 
-
-root_path = os.path.dirname(os.path.abspath(__file__))
-accounts_path = os.path.join(root_path, 'ACCOUNTS')
-success_path = os.path.join(root_path, 'SUCCESS')
-failed_path = os.path.join(root_path, 'FAILED')
-
-binary_model_path = os.path.join(root_path, 'binary_filter/models/binary_filter_model_1587241639.h5')
-categorizer_model_path = os.path.join(root_path, 'categorizer/models/my_model.h5')
-
-image_size_width = 150
-image_size_height = 150
-
-IMG_HEIGHT = 150
-IMG_WIDTH = 150
-
-
-binary_filter_classes = ['GIRL', 'LIT']
-categorizer_classes = ['ASIAN', 'BRUNETTE', 'DREADLOCK', 'GINGER', 'MULATTO']
-
-
+import config
 
 
 app_name = os.path.splitext(os.path.basename(__file__))[0]
+logger = config.get_logger(app_name)
 
-logger = logging.getLogger(app_name)
-formatter = logging.Formatter(fmt='%(asctime)s %(levelname)s %(name)s: %(message)s', datefmt='%y/%m/%d %H:%M:%S')
-streamHandler = logging.StreamHandler()
-streamHandler.setFormatter(formatter)
-logger.addHandler(streamHandler)
-logger.setLevel(logging.INFO)
-
-
-
-def get_paths(path):
-    return sorted(os.path.join(path, file) for file in os.listdir(path))
 
 
 def find_faces(account_path):
@@ -62,17 +35,17 @@ def find_faces(account_path):
             y1 += y_inc
 
         face_image = image[y0:(y1+y_inc), x0:(x1+x_inc)]
-        face_image_resized = cv2.resize(face_image, (image_size_width, image_size_height), interpolation=cv2.INTER_AREA)    
+        face_image_resized = cv2.resize(face_image, (config.IMG_WIDTH, config.IMG_HEIGHT), interpolation=cv2.INTER_AREA)    
         return face_image_resized
 
     logger.info(f'Handling account \'{os.path.basename(account_path)}\'')
     
     found_faces = []
 
-    file_paths = get_paths(account_path)
+    file_paths = config.get_paths(account_path)
     logger.info(f'Found {len(file_paths)} photos in account \'{os.path.basename(account_path)}\'')
 
-    for image_path in get_paths(account_path):
+    for image_path in config.get_paths(account_path):
         image = cv2.imread(image_path)
         height, width, channels = image.shape
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -82,15 +55,13 @@ def find_faces(account_path):
             gray,
             scaleFactor=1.3,
             minNeighbors=3,
-            minSize=(80, 80)
+            minSize=(config.IMG_MIN_WIDTH_FIND, config.IMG_MIN_HEIGHT_FIND)
         )
         for face in faces:
             found_faces.append((get_resized_face(face, image, height, width), image_path))
     
     logger.info(f'Found {len(found_faces)} faces in account \'{os.path.basename(account_path)}\'')
     return found_faces
-
-
 
 def get_prediction(model, image):
     image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
@@ -100,26 +71,34 @@ def get_prediction(model, image):
     image_tensor = np.reshape(image_tensor, [1, IMG_HEIGHT, IMG_WIDTH, 3])
     return model.predict_classes(image_tensor)[0]
 
+def move_account(account_path, prediction_status, prediction_class):
+    try:
+        if prediction_status == 'FAILED':
+            shutil.move(account_path, config.FAILED_PATH)
+        elif prediction_status == 'SUCCESS':
+            category_path = os.path.join(config.SUCCESS_PATH, config.CATEGORIZER_CLASSES[prediction_class])
+            #if not os.path.exists(category_path):
+            os.makedirs(category_path, exist_ok=True)
+            shutil.move(account_path, category_path)
+    except Exception as e:
+        logger.error(f'Eror while moving account {e}')
 
-def main():
+def predict(binary_model_path, categorizer_model_path):
 
     binary_filter = tf.keras.models.load_model(binary_model_path)
-    binary_filter.summary()
-
     categorizer = tf.keras.models.load_model(categorizer_model_path)
-    categorizer.summary()
 
-    accounts_paths = get_paths(accounts_path)
-    logger.info(f'Found {len(accounts_paths)} girls')
+    accounts_paths = config.get_paths(config.ACCOUNTS_PATH)
+    logger.info(f'Found {len(accounts_paths)} girls in {config.ACCOUNTS_PATH}')
 
     for account_path in accounts_paths:
         faces = find_faces(account_path)
 
         lit_counter = 0
-        predictions_statistics = [0] * len(categorizer_classes)
+        predictions_statistics = [0] * len(config.CATEGORIZER_CLASSES)
         
         for face in faces:
-            if binary_filter_classes[get_prediction(binary_filter, face[0])[0]] == 'LIT':
+            if config.BINARY_FILTER_CLASSES[get_prediction(binary_filter, face[0])[0]] == 'LIT':
                 logger.warning(f'Face \'{face[1]} is lit \'')
                 lit_counter += 1
                 continue
@@ -127,17 +106,30 @@ def main():
             predictions_statistics[get_prediction(categorizer, face[0])] += 1
 
         if lit_counter == len(faces):
-            logger.info(f'Move account \'{os.path.basename(account_path)}\' to FAILED')
-            # move_account(account_path, 'FAILED')
+            logger.warning(f'Move account \'{os.path.basename(account_path)}\' to FAILED')
+            move_account(account_path, 'FAILED')
         else:
             intensified_prediction = predictions_statistics.index(max(predictions_statistics))
             logger.info(predictions_statistics)
-            logger.info(f'Account \'{os.path.basename(account_path)}\' belongs to \'{categorizer_classes[intensified_prediction]}\' class')
+            logger.info(f'Account \'{os.path.basename(account_path)}\' belongs to \'{config.CATEGORIZER_CLASSES[intensified_prediction]}\' class')
+            move_account(account_path, 'SUCCESS', intensified_prediction)
 
-            #move_account(account_path, 'SUCCESS', intensified_prediction)
+def parse_args():
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument('--binary-filter-model', '-bfm', help='Path to binary-filter model', type=str, required=True)
+    parser.add_argument('--categorizer-model', '-cm', help='Path to categorizer model', type=str, required=True)
+    return parser.parse_args()
 
+def main():
+    args = parse_args()
 
+    binary_model_path = os.path.join(config.ROOT_PATH, 'binary_filter/models/binary_filter_model_1587241639.h5')
+    categorizer_model_path = os.path.join(config.ROOT_PATH, 'categorizer/models/my_model.h5')
+
+    # predict(args.binary_filter_model, categorizer_model)
+    predict(binary_model_path, categorizer_model_path)
 
 if __name__ == '__main__':
     main()
+
